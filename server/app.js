e9910ce8a10300f82c1feedcd17dfe9e88c7abc1
vishal-bangaru <vishalbangaru4@gmail.com>
2024-10-26 20:26:48 +0530

    second commit

--- server/app.js
+++ server/app.js
@@ -20,10 +20,10 @@ app.use((req, res, next) => {
 });
 
 
-    app.use('/', express.static(path.join(__dirname, './client/build')));
+    app.use('/', express.static(path.join(__dirname, '../client/build')));
   
     app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname,'./client/build/index.html'));
+      res.sendFile(path.join(__dirname,'../client/build/index.html'));
     });
 
 
